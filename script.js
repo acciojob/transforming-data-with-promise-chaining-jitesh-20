28fc0a069f2fffb570237414c558ba60df17381d
Jitesh Kumar <105545377+jitesh-20@users.noreply.github.com>
2025-02-04 20:19:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,52 @@
-//your JS code here. If required.
+document.getElementById("btn").addEventListener('click',()=>{
+	const num=document.getElementById("ip").value;
+	const outputDiv=document.getElementById("output");
+	if(isNaN(num)){
+		outputDiv.textContent="Please enter a valid number";
+		return;
+	}
+	new Promise((resolve)=>{
+		setTimeout(()=>{
+			outputDiv.textContent=`Result: ${num}`;
+			resolve(num);
+		},2000)
+	}).then((data1)=>{
+		return new Promise((resolve)=>{
+			setTimeout(()=>{
+				data1*=2;
+				outputDiv.textContent=`Result ${data1}`;
+				resolve(data1);
+			},2000)
+		})
+	}).then((data2)=>{
+		return new Promise((resolve)=>{
+			setTimeout(()=>{
+				data2-=3;
+				outputDiv.textContent=`Result ${data2}`;
+				resolve(data2);
+			},1000)
+		})
+	}).then((data3)=>{
+		return new Promise((resolve)=>{
+			setTimeout(()=>{
+				data3/=2;
+				outputDiv.textContent=`Result ${data3}`;
+				resolve(data3);
+			},1000)
+		})
+	}).then((data4)=>{
+		return new Promise((resolve)=>{
+			setTimeout(()=>{
+				data4+=10;
+				outputDiv.textContent=`Result ${data4}`;
+				resolve(data4);
+			},1000)
+		})
+	})
+})
+
+
+
+
+
+
